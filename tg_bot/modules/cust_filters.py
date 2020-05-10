9c92148fe61f221dae1ae4939c8738790146aff1
SUSHANT GIRDHAR <52759137+sushantgirdhar@users.noreply.github.com>
2020-05-10 18:58:29 +0530

    Update cust_filters.py

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -169,8 +169,14 @@ def reply_filter(bot: Bot, update: Update):
 
     chat_filters = sql.get_chat_triggers(chat.id)
     for keyword in chat_filters:
-        pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
-        if re.search(pattern, to_match, flags=re.IGNORECASE):
+        try:
+          pattern = r"( |^|[^\w])" + keyword + r"( |$|[^\w])"
+          match = re.search(pattern, to_match, flags=re.IGNORECASE)
+        except Exception:
+            message.reply_text(f"Removing filter {keyword} due to broken regex.")
+            sql.remove_filter(chat.id, keyword)
+            return
+        if match:
             filt = sql.get_filter(chat.id, keyword)
             if filt.is_sticker:
                 message.reply_sticker(filt.reply)
@@ -229,7 +235,6 @@ def __chat_settings__(chat_id, user_id):
 
 __help__ = """
  - /filters: list all active filters in this chat.
-
 *Admin only:*
  - /filter <keyword> <reply message>: add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
