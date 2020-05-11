991eb1f0612f155e98f5b3955abcfefcbd175035
sushantgirdhar1 <53021053+sushantgirdhar1@users.noreply.github.com>
2020-05-11 14:59:45 +0530

    Update welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -155,7 +155,7 @@ def new_member(bot: Bot, update: Update, job_queue: JobQueue):
 
             # Welcome yourself
             elif new_mem.id == bot.id:
-                update.effective_message.reply_text("Watashi ga kitta!", reply_to_message_id=reply)
+                update.effective_message.reply_text("Thanks for Adding me ! My Support Channel is @ebruiser ", reply_to_message_id=reply)
 
             else:
                 # If welcome message is media, send with appropriate function
