10b60dad0035ad6627b45f75a89562c2c493e349
sushantgirdhar1 <53021053+sushantgirdhar1@users.noreply.github.com>
2020-05-18 20:40:28 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -28,10 +28,7 @@ Follow @ebruiser if you want to keep up with the bot news and Check Basic Config
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-
-I am an Extra Ordinary group management bot, here to help you get around and keep the order in your groups with Extra Fun and Useful Modules.
-
-I have lots of handy features, such as warning system, a note keeping system, and even predetermined replies on certain keywords.
+I am an Extra Ordinary group management bot, here to help you get around and keep the order in your groups with Extra Fun and Useful Modules.I have lots of handy features, such as warning system, a note keeping system, and even predetermined replies on certain keywords.
 
 *Main* commands available:
  - /start: start the bot
