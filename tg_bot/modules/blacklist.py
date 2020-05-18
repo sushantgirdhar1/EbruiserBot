0ee4040a08763c2f6739496efce3946d9c7c6635
sushantgirdhar1 <53021053+sushantgirdhar1@users.noreply.github.com>
2020-05-18 16:57:05 +0530

    Update blacklist.py

--- tg_bot/modules/blacklist.py
+++ tg_bot/modules/blacklist.py
@@ -180,18 +180,27 @@ def __stats__():
                                                             sql.num_blacklist_filter_chats())
 
 
-__help__ = """
-Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, \
+__help__ = f"""
+Blacklists Words are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, \
 the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
 *NOTE:* blacklists do not affect group admins.
  - /blacklist: View the current blacklisted words.
- 
 *Admin only:*
  - /addblacklist <triggers>: Add a trigger to the blacklist. Each line is considered one trigger, so using different \
 lines will allow you to add multiple triggers.
  - /unblacklist <triggers>: Remove triggers from the blacklist. Same newline logic applies here, so you can remove \
 multiple triggers at once.
  - /rmblacklist <triggers>: Same as above.
+ 
+Here is the Help for *URL* or *Domain Blacklisting*
+
+ Domain blacklisting is used to stop certain domains from being mentioned in a group, Any time an url on that domain is mentioned, /
+the message will immediately be deleted.
+*NOTE:* domain blacklisting do not affect group admins.
+- /geturl: View the current blacklisted urls
+*Admin only:*
+- /addurl <urls>: Add a domain to the blacklist. The bot will automatically parse the url.
+- /delurl <urls>: Remove urls from the blacklist
 """
 
 BLACKLIST_HANDLER = DisableAbleCommandHandler("blacklist", blacklist, pass_args=True)
