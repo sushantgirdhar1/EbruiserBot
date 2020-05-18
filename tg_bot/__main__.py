3d62d2b637c7ee03c32c3932ca9b450ea36ad882
sushantgirdhar1 <53021053+sushantgirdhar1@users.noreply.github.com>
2020-05-18 19:40:44 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -24,7 +24,7 @@ Made with love by @sushantgirdhar 💫
 
 See My [Basic Configuration Commands](t.me/ebruiser/58)
 
-Follow {}. if you want to keep up with the bot news, bot updates and bot downtime!
+Follow @ebruiser if you want to keep up with the bot news, bot updates and bot downtime!
 """
 
 HELP_STRINGS = """
@@ -140,7 +140,8 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="➕ Add me to a Group ➕",
                                                                        url="t.me/{}?startgroup=true".format(bot.username))]]))
-
+                 keyboard += [[InlineKeyboardButton(text="🛠 Reporting", url="t.me/ebruiser/"), 
+        InlineKeyboardButton(text="❔ Help", url="t.me/ebruiser/")]]             
 
     else:
         update.effective_message.reply_text("Yo,Wassup?")
