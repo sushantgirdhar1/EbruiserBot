5df898e2a4ca403a0fb4ccc9eb03a6aba6976d82
sushantgirdhar1 <53021053+sushantgirdhar1@users.noreply.github.com>
2020-05-18 09:04:48 +0530

    Update covidindia.py

--- tg_bot/modules/covidindia.py
+++ tg_bot/modules/covidindia.py
@@ -46,7 +46,6 @@ def covindia(bot: Bot, update: Update):
 __help__ = """
  - /covindia <state>: Get real time COVID-19 stats for the input Indian state
  - /cov <country> <state> <locality>: Globally Get real time COVID-19 stats for the input location.
- - /cov top <n(integer)>: Get the top n countries with the highest confirmed cases.
  - /corona : Gets a random Argument from the bot
  - /covid get worldwide corona status
 """
