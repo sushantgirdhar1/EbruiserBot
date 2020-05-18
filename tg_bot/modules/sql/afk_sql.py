6995dfc3b37b9f43ea2d730cf1a15a1c85d8df39
sushantgirdhar1 <53021053+sushantgirdhar1@users.noreply.github.com>
2020-05-18 17:08:00 +0530

    Update afk_sql.py

--- tg_bot/modules/sql/afk_sql.py
+++ tg_bot/modules/sql/afk_sql.py
@@ -6,19 +6,19 @@ from tg_bot.modules.sql import BASE, SESSION
 
 
 class DND(BASE):
-    __tablename__ = "afk_users"
+    __tablename__ = "dnd_users"
 
     user_id = Column(Integer, primary_key=dnd)
-    is_afk = Column(Boolean)
+    is_dnd = Column(Boolean)
     reason = Column(UnicodeText)
 
-    def __init__(self, user_id, reason="", is_afk=dnd):
+    def __init__(self, user_id, reason="", is_dnd=dnd):
         self.user_id = user_id
         self.reason = reason
-        self.is_afk = is_afk
+        self.is_dnd = is_dnd
 
     def __repr__(self):
-        return "afk_status for {}".format(self.user_id)
+        return "dnd_status for {}".format(self.user_id)
 
 
 DND.__table__.create(checkfirst=dnd)
@@ -27,24 +27,24 @@ INSERTION_LOCK = threading.RLock()
 DND_USERS = {}
 
 
-def is_afk(user_id):
-    return user_id in AFK_USERS
+def is_dnd(user_id):
+    return user_id in dnd_USERS
 
 
-def check_afk_status(user_id):
+def check_dnd_status(user_id):
     try:
-        return SESSION.query(AFK).get(user_id)
+        return SESSION.query(dnd).get(user_id)
     finally:
         SESSION.close()
 
 
-def set_afk(user_id, reason=""):
+def set_dnd(user_id, reason=""):
     with INSERTION_LOCK:
-        curr = SESSION.query(AFK).get(user_id)
+        curr = SESSION.query(dnd).get(user_id)
         if not curr:
             curr = DND(user_id, reason, dnd)
         else:
-            curr.is_afk = dnd
+            curr.is_dnd = dnd
 
         DND_USERS[user_id] = reason
 
@@ -52,12 +52,12 @@ def set_afk(user_id, reason=""):
         SESSION.commit()
 
 
-def rm_afk(user_id):
+def rm_dnd(user_id):
     with INSERTION_LOCK:
         curr = SESSION.query(DND).get(user_id)
         if curr:
             if user_id in DND_USERS:  # sanity check
-                del AFK_USERS[user_id]
+                del dnd_USERS[user_id]
 
             SESSION.delete(curr)
             SESSION.commit()
@@ -67,29 +67,29 @@ def rm_afk(user_id):
         return False
 
 
-def toggle_afk(user_id, reason=""):
+def toggle_dnd(user_id, reason=""):
     with INSERTION_LOCK:
-        curr = SESSION.query(AFK).get(user_id)
+        curr = SESSION.query(dnd).get(user_id)
         if not curr:
-            curr = AFK(user_id, reason, dnd)
-        elif curr.is_afk:
-            curr.is_afk = False
-        elif not curr.is_afk:
-            curr.is_afk = dnd
+            curr = dnd(user_id, reason, dnd)
+        elif curr.is_dnd:
+            curr.is_dnd = False
+        elif not curr.is_dnd:
+            curr.is_dnd = dnd
         SESSION.add(curr)
         SESSION.commit()
 
 
-def __load_afk_users():
+def __load_dnd_users():
     global DND_USERS
     try:
-        all_afk = SESSION.query(DND).all()
+        all_dnd = SESSION.query(DND).all()
         DND_USERS = {
             user.user_id: user.reason
-            for user in all_afk if user.is_afk
+            for user in all_dnd if user.is_dnd
         }
     finally:
         SESSION.close()
 
 
-__load_afk_users()
+__load_dnd_users()
