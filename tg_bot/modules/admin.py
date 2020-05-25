618d64db4d94fd710921bba4131198e26cf99e7c
sushantgirdhar1 <53021053+sushantgirdhar1@users.noreply.github.com>
2020-05-25 18:54:55 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -355,7 +355,7 @@ dispatcher.add_handler(PROMOTE_HANDLER)
 dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 
-__mod_name__ = "Admin"
+__mod_name__ = "Admin👨🏻‍⚖"
 __command_list__ = ["adminlist", "admins", "invitelink", "promote", "demote"]
 __handlers__ = [ADMINLIST_HANDLER, PIN_HANDLER, UNPIN_HANDLER,
                 INVITE_HANDLER, PROMOTE_HANDLER, DEMOTE_HANDLER, SET_TITLE_HANDLER]
