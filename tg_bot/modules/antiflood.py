482b2dc45e09f7b8897142d92b5674d7fbfdc764
sushantgirdhar1 <53021053+sushantgirdhar1@users.noreply.github.com>
2020-05-26 15:44:26 +0530

    Update antiflood.py

--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -166,5 +166,5 @@ dispatcher.add_handler(FLOOD_BAN_HANDLER, FLOOD_GROUP)
 dispatcher.add_handler(SET_FLOOD_HANDLER)
 dispatcher.add_handler(FLOOD_HANDLER)
 
-__mod_name__ = "AntiFlood🌀"
+__mod_name__ = "AntiFlood"
 __handlers__ = [(FLOOD_BAN_HANDLER, FLOOD_GROUP), SET_FLOOD_HANDLER, FLOOD_HANDLER]
