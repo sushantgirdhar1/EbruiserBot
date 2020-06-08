0e8fe85c60c2bae74299081243224e2955caf4a9
SUSHANT <53021053+sushantgirdhar1@users.noreply.github.com>
2020-06-08 22:08:58 +0530

    Update panel_string.py

--- tg_bot/modules/helper_funcs/panel_string.py
+++ tg_bot/modules/helper_funcs/panel_string.py
@@ -1,19 +1,26 @@
 tg_bot_IMG = "https://telegra.ph/file/7699c3a2c49b799c7be59.png"
 
 SOURCE_STRING = """
-A Python Based , Telegram Group Management Bot , Another Marie Fork and am fully opensource - you can find what makes me tick [here](https://github.com/sushantgirdhar/EbruiserBot)
+*#GITHUB*
+[GitHub Repo](github.com/sushantgirdhar/EbruiserBot) , Don't forget to give a star ⭐.
+
+*#CLONE_GUIDE*
+[Click Here for the guide](t.me/ebruiser/106)
+
+Please don't fuckin spam my master inbox for help ,Instead join group with @ebruiser.Give Proper logs before asking anything.
+
+*#NOTE* if you are not that pro and not able to clone , even after the guide . 
+just use me - /start . Instead of cloning
 """
 
 HELP = """
 Hey there CA Aspirant! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
+I'm a group management bot made for CA groups only! Have a look at the following for an idea of some of \
 the things I can help you with.
 
 *Main* commands available:
  - /start: start the bot
  - /help: PM's you this message.
-
-
 {}
 And the following:
 """
