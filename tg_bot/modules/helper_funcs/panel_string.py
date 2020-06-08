417d6e4fe1df5f4d7a1db484c029a6491c6be920
SUSHANT <53021053+sushantgirdhar1@users.noreply.github.com>
2020-06-08 21:42:34 +0530

    Create panel_string.py

---
+++ tg_bot/modules/helper_funcs/panel_string.py
@@ -0,0 +1,19 @@
+tg_bot_IMG = "https://telegra.ph/file/7699c3a2c49b799c7be59.png"
+
+SOURCE_STRING = """
+A Python Based , Telegram Group Management Bot , Another Marie Fork and am fully opensource - you can find what makes me tick [here](https://github.com/sushantgirdhar/EbruiserBot)
+"""
+
+HELP = """
+Hey there CA Aspirant! My name is *{}*.
+I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
+the things I can help you with.
+
+*Main* commands available:
+ - /start: start the bot
+ - /help: PM's you this message.
+
+
+{}
+And the following:
+"""
